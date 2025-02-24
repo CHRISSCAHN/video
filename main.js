d837a4f796a58b1c87f95e6e21ec2e494b2fe7c1
CHRISSCAHN <123827382+CHRISSCAHN@users.noreply.github.com>
2025-02-24 19:05:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ class VideoConference {
 
     initializeWebSocket() {
         // Use your deployed WebSocket server URL here
-        const wsUrl = 'https://video-rg6u.onrender.com';
+        const wsUrl = 'wss://video-rg6u.onrender.com';
         
         if (this.ws?.readyState === WebSocket.OPEN) {
             this.ws.close();
